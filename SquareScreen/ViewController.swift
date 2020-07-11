2d2b0b601023926cc0e5d6e9f7f3c2f448909ef6
roman dod <stalkeroma1@mail.ru>
2020-07-11 16:15:05 +0300

    Initial Commit

---
+++ SquareScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquareScreen
+//
+//  Created by Roman Dod on 7/11/20.
+//  Copyright © 2020 Roman Dod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
